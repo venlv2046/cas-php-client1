e9cbb1b72af1a51ce9702aaac9026da0e24bcff8
venlv2046 <venlv2046@gmail.com>
2017-04-08 11:47:53 +0800

    init

---
+++ fuel/app/logs/2017/04/08.php
@@ -0,0 +1,5 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2017-04-08 10:57:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-04-08 10:57:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-04-08 11:47:13 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
